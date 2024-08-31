ee690722771f6e99bab7dd1649c6c3b2b802e393
Ravali Kalidas <ravali.kalidas@gmail.com>
2024-08-31 21:52:02 +0530

    ravali'

---
+++ Home.js
@@ -0,0 +1,13 @@
+
+function navigateToHome () {
+    window.location.assign("Home.html")
+}
+function navigateToFeature () {
+    window.location.assign("Features.html")
+}
+function navigateToServices () {
+    window.location.assign("Services.html")
+}
+function navigateToContact () {
+    window.location.assign("Contact.html")
+}
